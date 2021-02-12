ad2c9524976ae9866d6b00c38f5bd2489b16a21b
Tristan Garzon <47580382+tristangarzon@users.noreply.github.com>
2021-02-12 18:30:17 -0500

    Update to main Camera
    Added a lot more functionality to the camera. It acts very similar to a camera found in popular RTS style games.
    Movement:
    - "WASD" or "Arrow Keys"
    - Mouse Dragable
    - Border Pan
    
    Zoom:
    - Mouse scroll wheel
    
    Rotate:
    - Mouse middle button
    (the mouse rotation still needs some work)

--- Project004/Assets/Scripts/CamController.cs
+++ Project004/Assets/Scripts/CamController.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 
 public class CamController : MonoBehaviour
 {
+    [Header("Main Camera")]
+    public Transform cameraTransform;
+
+    [Header("Movement")]
     public float movementSpeed;
     public float movementTime;
-    public float rotationAmount;
-    public float panBorderThickness;
-
     public Vector3 newPosition;
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
+
+    [Header("Rotation")]
+    public float rotationAmount;
     public Quaternion newRotation;
+    public Vector3 rotateStartPosition;
+    public Vector3 rotateCurrentPosition;
 
+    [Header("Zoom")]
+    public Vector3 zoomAmount;
+    public float minZoom;
+    public float maxZoom;
+    public Vector3 newZoom;
 
+    [Header("Pan")]
+    public float panBorderThickness;
+    public Vector2 panLimit;
+   
   
     void Start()
     {
         newPosition = transform.position;
         newRotation = transform.rotation;
+        newZoom = cameraTransform.localPosition;
     }
 
     void Update()
@@ -51,6 +69,7 @@ public class CamController : MonoBehaviour
             newPosition += (transform.right * -movementSpeed);
         }
 
+        /*
         //Rotate the Camera ("Q" & "W")
         //Clockwise
         if (Input.GetKey(KeyCode.Q))
@@ -62,7 +81,7 @@ public class CamController : MonoBehaviour
         {
             newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
         }
-
+        */
 
         //Smooths the movement of the camera
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
@@ -74,7 +93,20 @@ public class CamController : MonoBehaviour
 
     void HandlesMouseMovementInput()
     {
-        //Moves the camera ("Mouse")
+        //Zoom
+        if(Input.mouseScrollDelta.y != 0)
+        {
+            if (Input.mouseScrollDelta.y > 0 && newZoom.z < maxZoom)
+            {
+                newZoom += Input.mouseScrollDelta.y * zoomAmount;
+            }
+            if (Input.mouseScrollDelta.y < 0 && newZoom.z > minZoom)
+            {
+                newZoom += Input.mouseScrollDelta.y * zoomAmount;
+            }
+        }
+
+        //Moves the camera edge("Mouse")
         //Up
         if (Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
@@ -96,8 +128,63 @@ public class CamController : MonoBehaviour
             newPosition += (transform.right * -movementSpeed);
         }
 
+        //Moves the camera drag("Mouse")
+        if (Input.GetMouseButtonDown(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if(plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+
+                newPosition = transform.position + dragStartPosition - dragCurrentPosition; 
+            }
+        }
+
+        //Rotates the camera("Mouse")
+        if (Input.GetMouseButtonDown(2))
+        {
+            rotateStartPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(2))
+        {
+            rotateCurrentPosition = Input.mousePosition;
+
+            float dx = (rotateCurrentPosition - rotateStartPosition).x * rotationAmount;
+            float dy = (rotateCurrentPosition - rotateStartPosition).y * rotationAmount;
+            float dz = (rotateCurrentPosition - rotateStartPosition).z * rotationAmount;
+
+
+            transform.rotation *= Quaternion.Euler(new Vector3(0, dx, dz));
+
+        }
+
+        //Limits how far the camera is able to move/zoom
+        newPosition.x = Mathf.Clamp(newPosition.x, -panLimit.x, panLimit.x);
+        newPosition.z = Mathf.Clamp(newPosition.z, -panLimit.y, panLimit.y);
+     
+
         //Smooths the movement of the camera
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
     }
 
 }
